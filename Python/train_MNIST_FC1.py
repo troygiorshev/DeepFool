d0b5e0ad775180fc537934e1b0e4e9943b30d545
Jessica Landon <15jdl4@queensu.ca>
2020-04-06 19:11:02 -0400

    Got rid of batch dimension in input.

--- Python/train_MNIST_FC1.py
+++ Python/train_MNIST_FC1.py
@@ -4,6 +4,8 @@ import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 
 
 class Net(nn.Module):
@@ -94,11 +96,13 @@ def main():
         test_loss = 0
         correct = 0
         with torch.no_grad():
-            for data, target in test_loader:
-                output = network(data)
-                test_loss += F.nll_loss(output, target, size_average=False).item()
-                pred = output.data.max(1, keepdim=True)[1]
-                correct += pred.eq(target.data.view_as(pred)).sum()
+            for batch_idx, (data,target) in enumerate(test_loader):
+                for data, target in zip(data,target):
+                    print(data.shape)
+                    output = network(data)
+                    test_loss += F.nll_loss(output, target, size_average=False).item()
+                    pred = output.data.max(1, keepdim=True)[1]
+                    correct += pred.eq(target.data.view_as(pred)).sum()
         test_loss /= len(test_loader.dataset)
         test_losses.append(test_loss)
         print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
@@ -113,3 +117,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
