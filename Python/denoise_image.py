cd9c6122dbb5e25ced49ed6c2184882e1d621eff
Troy Giorshev <troygiorshev@gmail.com>
2020-04-06 17:09:01 -0400

    Make denoise_image.py multithreaded

--- Python/denoise_image.py
+++ Python/denoise_image.py
@@ -6,6 +6,10 @@ import glob
 from PIL import Image
 from PIL import ImageFilter
 import progressbar
+import multiprocessing
+
+
+NUM_PROC = 6
 
 
 def denoiseColor(filepath):
@@ -39,47 +43,68 @@ def bilateralfilter(filepath):
 
 
 def adjust_images(input_path, output_folder):
-    files = [f for f in glob.glob(input_path + "**/*.jpeg", recursive=True)]
+    files_all = [f for f in glob.glob(input_path + "**/*.jpeg", recursive=True)]
+    num_files = len(files_all)
 
-    bar = progressbar.ProgressBar(maxval=len(files)).start()
-    i=1
+    proc_list = []
+    size = num_files // num_proc
+
+    for i in range(num_proc):
+        if i != num_proc - 1:
+            files_part = files_all[i*size:(i+1)*size]
+        else:
+            files_part = files_all[i*size:]
+        p = multiprocessing.Process(target=do_work, args=(files_part, output_folder))
+        proc_list.append(p)
+
+    for p in proc_list:
+        p.start()
+
+    # Wait for them all to be done before exiting
+    for p in proc_list:
+        p.join()
+
+
+def do_work(files, output_folder):
     for path in files:
         filename = os.path.basename(path)
 
         img = denoiseColor(path) 
-        savefile='../data/ILSVRC2012_img_val/'+output_folder+'/denoised/'
+        savefile='../data/ILSVRC2012_img_val/'+output_folder+'denoised/'
         cv2.imwrite(savefile+os.path.basename(path), img)
 
         img = sharpen(path)
-        savefile='../data/ILSVRC2012_img_val/'+output_folder+'/sharpen/'
+        savefile='../data/ILSVRC2012_img_val/'+output_folder+'sharpen/'
         img.save(savefile+os.path.basename(path))
 
         img = bilateralfilter(path) 
-        savefile='../data/ILSVRC2012_img_val/'+output_folder+'/bilateralfilter/'
+        savefile='../data/ILSVRC2012_img_val/'+output_folder+'bilateralfilter/'
         cv2.imwrite(savefile+filename, img)
 
         img = gaussianblur(path)
-        savefile='../data/ILSVRC2012_img_val/'+output_folder+'/gaussianblur/'
+        savefile='../data/ILSVRC2012_img_val/'+output_folder+'gaussianblur/'
         cv2.imwrite(savefile+filename, img)
 
         img = medianblur(path)
-        savefile='../data/ILSVRC2012_img_val/'+output_folder+'/medianblur/'
-        cv2.imwrite(savefile+filename, img)
-        
-        i = i+1
-        bar.update(i)
+        savefile='../data/ILSVRC2012_img_val/'+output_folder+'medianblur/'
+        cv2.imwrite(savefile+filename, img) 
 
 
 def main():
     # path to perturbed images
     input_path = '../data/ILSVRC2012_img_val/perturbed/'
-    output_folder = 'perturbedModification'
+    output_folder = 'perturbedModification/'
     adjust_images(input_path, output_folder)
+
+    print("Finished Perturbed Images")
 
     input_path = '../data/ILSVRC2012_img_val/raw/'
-    output_folder = 'originalImgModification'
+    output_folder = 'originalImgModification/'
     adjust_images(input_path, output_folder)
 
+    print("Finished All Images")
+
 if __name__=='__main__':
+    num_proc = int(input("Number of Threads: "))
     main()
 
