455d6c2c70e49103ac64687cc1c975e1114d8706
chels <chelsey.kurylo@gmail.com>
2020-04-06 20:35:51 -0400

    MNIST manipulation of perturbed images

--- Python/denoise_image.py
+++ Python/denoise_image.py
@@ -1,12 +1,14 @@
-import numpy as np 
-import cv2 
-from matplotlib import pyplot as plt 
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
 #import imageio
 import os
 import glob
 from PIL import Image
 from PIL import ImageFilter
 import progressbar
+import keras
 
 """
 filepath = "../data/ILSVRC2012_img_val/ILSVRC2012_val_00000001.JPEG"
@@ -24,14 +26,17 @@ plt.subplot(122), plt.imshow(dst)
   
 plt.show()
 """
+
+
 def denoiseColor(filepath):
-    img = cv2.imread(filepath) 
-    dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15) 
+    img = cv2.imread(filepath)
+    dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 15)
     return dst
 
+
 def denoiseGrayscale(filepath):
-    img = cv2.imread(filepath) 
-    dst = cv2.fastNlMeansDenoising(img, None, 10, 10, 7, 15) 
+    img = cv2.imread(filepath)
+    dst = cv2.fastNlMeansDenoising(img, None, 10, 10, 7, 15)
     return dst
 
 
@@ -43,37 +48,55 @@ def sharpen(filepath):
 
 def gaussianblur(filepath):
     img = cv2.imread(filepath)
-    blur = cv2.GaussianBlur(img,(5,5),0)
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
     return blur
 
 
 def medianblur(filepath):
     img = cv2.imread(filepath)
-    blur = cv2.medianBlur(img,5)
+    blur = cv2.medianBlur(img, 5)
     return blur
 
 
 def bilateralfilter(filepath):
     img = cv2.imread(filepath)
-    blur = cv2.bilateralFilter(img,9,75,75)
+    blur = cv2.bilateralFilter(img, 9, 75, 75)
     return blur
 
 
-def adjust_images(input_path, output_folder):
+def MNISTdae(filepath, dae):
+    img = cv2.imread(filepath)
+    # plt.imshow(img)
+    # plt.show()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img[2:-2, 2:-2] / 255.0
+    img = img.reshape(-1, 28, 28, 1)
+    #plt.imshow(np.squeeze(img), cmap=cm.gray)
+    #plt.show()
+    encodedImg = dae.predict(img)
+    encodedImg = np.squeeze(encodedImg)
+    #plt.imshow(encodedImg, cmap=cm.gray)
+    #plt.show()
+    return encodedImg*255.0
+
+
+def adjust_images(input_path, output_folder, dae=None):
     files = [f for f in glob.glob(input_path + "**/*.jpeg", recursive=True)]
 
     bar = progressbar.ProgressBar(maxval=len(files)).start()
-    i=1
+    i = 0
     for path in files:
         filename = os.path.basename(path)
 
+        """
+        #IMAGENET Manipulations
         img = denoiseColor(path) 
         savefile='../data/ILSVRC2012_img_val/'+output_folder+'/denoised/'
-        cv2.imwrite(savefile+os.path.basename(path), img)
+        cv2.imwrite(savefile+filename, img)
 
         img = sharpen(path)
         savefile='../data/ILSVRC2012_img_val/'+output_folder+'/sharpen/'
-        img.save(savefile+os.path.basename(path))
+        img.save(savefile+filename)
 
         img = bilateralfilter(path) 
         savefile='../data/ILSVRC2012_img_val/'+output_folder+'/bilateralfilter/'
@@ -86,12 +109,42 @@ def adjust_images(input_path, output_folder):
         img = medianblur(path)
         savefile='../data/ILSVRC2012_img_val/'+output_folder+'/medianblur/'
         cv2.imwrite(savefile+filename, img)
-        
+        """
+
+        #MNIST Manipulations
+
+        img = MNISTdae(path, dae)
+        savefile = '../data/MNIST/'+output_folder+'/dae/'
+        cv2.imwrite(savefile+filename, img)
+
+        img = denoiseColor(path) 
+        savefile='../data/MNIST/'+output_folder+'/denoised/'
+        cv2.imwrite(savefile+filename, img)
+
+
+        img = bilateralfilter(path) 
+        savefile='../data/MNIST/'+output_folder+'/bilateralfilter/'
+        cv2.imwrite(savefile+filename, img)
+
+        img = gaussianblur(path)
+        savefile='../data/MNIST/'+output_folder+'/gaussianblur/'
+        cv2.imwrite(savefile+filename, img)
+
+        img = medianblur(path)
+        savefile='../data/MNIST/'+output_folder+'/medianblur/'
+        cv2.imwrite(savefile+filename, img)
+
+        img = sharpen(path)
+        savefile='../data/MNIST/'+output_folder+'/sharpen/'
+        img.save(savefile+filename)
+
         i = i+1
         bar.update(i)
 
 
 def main():
+    """
+    #IMAGENET
     # path to perturbed images
     input_path = '../data/perturbed/1/'
     output_folder = 'perturbedModification'
@@ -100,7 +153,15 @@ def main():
     input_path = '../data/ILSVRC2012_img_val/'
     output_folder = 'originalImgModification'
     adjust_images(input_path, output_folder)
+    """
 
-if __name__=='__main__':
+    #MNIST
+    dae = keras.models.load_model('dae_mnist_autoencoder.h5')
+
+    input_path = '../data/MNIST/perturbed/'
+    output_folder = 'perturbedModification'
+    adjust_images(input_path, output_folder, dae)
+
+
+if __name__ == '__main__':
     main()
-
